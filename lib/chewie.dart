8df87d5c5c760a620089cf103c290f1c3568d743
d.kadakauskis <d.kadakauskis@gmail.com>
2024-10-22 08:31:51 +0300

    imports

--- lib/chewie.dart
+++ lib/chewie.dart
@@ -7,3 +7,10 @@ export 'src/material/material_controls.dart';
 export 'src/material/material_progress_bar.dart';
 export 'src/material/material_desktop_controls.dart';
 export 'src/models/index.dart';
+export 'src/material/widgets/options_dialog.dart';
+export 'src/center_seek_button.dart';
+export 'src/center_play_button.dart';
+export 'src/animated_play_pause.dart';
+export 'src/material/widgets/playback_speed_dialog.dart';
+export 'src/helpers/utils.dart';
+
